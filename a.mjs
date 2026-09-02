f731a56f5bf9a2c3084fd037e104059267760072
Htein Lynn Aung <hteinlynnaung.1111@gmail.com>
2026-09-02 23:35:08 +0700

    Update MyCode

---
+++ a.mjs
@@ -0,0 +1,12 @@
+// ES Module (ESM) - a.mjs
+/* Module is using the connection between codefile */
+
+export const PI = 3.14; // export two function to the b.mjs
+
+export function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
---
+++ b.mjs
@@ -0,0 +1,6 @@
+// ES Module (ESM) - b.mjs
+
+import { PI, add } from "./a.mjs"; // calling the a.mjs
+
+console.log(PI);
+console.log(add(1, 2)); // run output in terminal using command -> node b.mjs
